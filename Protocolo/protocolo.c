3ace55a0ece26d0c39994c5520d086ff3b49e185
MartinIMR <martin.mimr@gmail.com>
2018-09-02 21:31:21 -0500

    Programa de emulacion de protocolo funcionando!

--- Protocolo/protocolo.c
+++ Protocolo/protocolo.c
@@ -15,7 +15,7 @@ main(void)
 {
  FILE * archivo_cadenas, * archivo_aceptadas;
  char ** data = NULL; 
- int n = 10, m = 10, generacion = 1;
+ int n = 100, m = 100000, generacion = 1;
  archivo_cadenas = fopen("data.txt","w");
  archivo_aceptadas = fopen("accepted.txt","w");
  if(archivo_cadenas == NULL || archivo_aceptadas == NULL)
@@ -23,24 +23,26 @@ main(void)
   printf("Un archivo no pudo ser abierto\nSaliendo...\n");
   return -1;
  }
- while(aleatorio(1))
- {
+
+ while( aleatorio(1) != 0)
+ { 
   fprintf(archivo_cadenas,"------- Generacion %d -------\n",generacion); 
   data = crear_datos(archivo_cadenas,n,m);
   fprintf(archivo_cadenas,"------- Fin generacion %d -------\n",generacion); 
   #if defined(_WIN32) || defined(_WIN64)
    Sleep(2000);
   #else
-   sleep(2000);
+   sleep(2);
   #endif
   fprintf(archivo_aceptadas,"------- Filtradas %d -------\n",generacion);
   filtrar(archivo_aceptadas,data,n);
   fprintf(archivo_aceptadas,"------- Fin filtradas %d -------\n",generacion);
   free(data);
   generacion++;
- }
+ } 
  fclose(archivo_cadenas);
  fclose(archivo_aceptadas);
+
  return 0;
 }
 
